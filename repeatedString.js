aaa6b86b839af9bffbb9f24f4334e5b3253aac3e
Nelli Butenko <butenkonelly93@gmail.com>
2021-08-21 16:06:47 -0400

    loops to create a long string and count a occurances

--- repeatedString.js
+++ repeatedString.js
@@ -1,6 +1,9 @@
 const s = 'aba';
 const n = 10; // 7
 
+const s1 = 'a';
+const n1 = 100000000;
+
 
 function repeatedString(s, n) {
   
@@ -9,4 +12,27 @@ function repeatedString(s, n) {
   // check length with 'n' after each concatenation
   // when length === n, count char 'a' in long string ( with match() or split().length - 1)
 
+  let longString = s;
+
+  if (longString.length === 1) {
+    return n;
+  }
+
+  for (let i = 0; i < s.length; i++ ) {
+    if(longString.length < n) {
+      longString += s[i];
+      if (i === s.length - 1) {
+        i = -1;
+      } 
+    }
+  }
+  if (longString.length === n) {
+    const numberOfA = longString.split('a').length - 1;
+    return numberOfA;
+  }
 }
+
+
+console.log (repeatedString(s1, n1))
+
+// not finished 6/23 tests :(
