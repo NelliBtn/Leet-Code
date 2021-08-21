75950128a43dde7e740ad607bcc3e8df5c38b190
Nelli Butenko <butenkonelly93@gmail.com>
2021-08-21 15:32:04 -0400

    pseudo code

---
+++ repeatedString.js
@@ -0,0 +1,12 @@
+const s = 'aba';
+const n = 10; // 7
+
+
+function repeatedString(s, n) {
+  
+  // create a long string variable that is equal to single string to start with
+  // iterate the original string to add characters one by one
+  // check length with 'n' after each concatenation
+  // when length === n, count char 'a' in long string ( with match() or split().length - 1)
+
+}
