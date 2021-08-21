dab41cb3ba4735171835f574d266167863d541e8
Nelli Butenko <butenkonelly93@gmail.com>
2021-08-21 15:22:35 -0400

    cloud game / hacker rank

--- jumpingOnClouds.js
+++ jumpingOnClouds.js
@@ -1,15 +1,26 @@
-// const n = 6;
+/**
+ * Determine the minimum number of jumps it will take to jump from the starting postion to the last cloud. 
+ * It is always possible to win the game.
+ * For each game, you will get an array of clouds numbered  if they are safe or  if they must be avoided.
+ */
+
 const array = [0, 0, 0, 0, 1, 0];
 
-
 function jumpingOnClouds(c) {
-  // count = 0
-  // current cloud = index 0
-  // iterate through array
-  // if [i+2] = 0, then current cloud index = [i+2], count ++
-  // if [i+1] = 0, then current cloud index = [i+1], count ++
-  // if both = 1, you lost
-  // return count
+
+  let countJumps = 0;
+
+  for (let i = 0; i < c.length; i++) {
+    if (c[i + 2] === 0) { // if it's safe to jump 2 clouds
+      i++; // increment index +1, it will incremenet another +1 on the next loop
+      countJumps++; // increment jump count
+    } else if (c[i + 1] === 0) { // check if it is safe to jump 1 cloud and it's not undefined
+      countJumps++; // increment jump count
+    }
+    // i++
+  }
+
+  return countJumps;
 }
 
 console.log(jumpingOnClouds(array))
