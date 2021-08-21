40596e4f0a580d6915badd103ae74705b04ead30
Nelli Butenko <butenkonelly93@gmail.com>
2021-08-21 15:04:12 -0400

    write pseudo code

---
+++ jumpingOnClouds.js
@@ -0,0 +1,15 @@
+// const n = 6;
+const array = [0, 0, 0, 0, 1, 0];
+
+
+function jumpingOnClouds(c) {
+  // count = 0
+  // current cloud = index 0
+  // iterate through array
+  // if [i+2] = 0, then current cloud index = [i+2], count ++
+  // if [i+1] = 0, then current cloud index = [i+1], count ++
+  // if both = 1, you lost
+  // return count
+}
+
+console.log(jumpingOnClouds(array))
